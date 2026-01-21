e0bc4b734037ef2c14fb41028fda25ed549eb344
cloud-with-preetham <preetham.pereira@icloud.com>
2026-01-21 10:15:00 +0530

    feat(day-08): define basic S3 bucket stack using AWS CDK

---
+++ 15-programming/python-for-devops/day-08/cdk-demo/cdk_demo_stack.py
@@ -0,0 +1,11 @@
+from aws_cdk import Stack
+from aws_cdk import aws_s3 as s3
+from constructs import Construct
+
+
+class CdkDemoStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        s3.Bucket(self, "DemoBucket", versioned=True)
