fe5704277c6fe2b2650a7546ccd0b58780906cf8
cloud-with-preetham <preetham.pereira@icloud.com>
2026-03-24 11:00:00 +0530

    feat(terraform): provision AWS network infrastructure EC2 instance and S3 bucket

---
+++ 11-infrastructure-as-code/terraform/day-62/main.tf
@@ -0,0 +1,110 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "TerraWeek-VPC"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "TerraWeek-Public-Subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "TerraWeek-IGW"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "TerraWeek-RouteTable"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "main" {
+  name        = "terraweek-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TerraWeek-SG"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami           = "ami-02167eae61967e403"
+  instance_type = "t3.micro"
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.main.id]
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "TerraWeek-Server"
+  }
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "terraweek-logs-preetham-2026"
+
+  depends_on = [
+    aws_instance.main
+  ]
+
+  tags = {
+    Name = "TerraWeek-Logs"
+  }
+}
