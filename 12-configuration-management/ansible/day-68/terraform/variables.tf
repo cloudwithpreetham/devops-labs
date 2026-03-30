980debe61150f13dd5c6c4028bf90a4aac315e84
cloud-with-preetham <preetham.pereira@icloud.com>
2026-03-30 09:05:00 +0530

    feat(terraform): configure modular infrastructure with workspace-aware settings

---
+++ 12-configuration-management/ansible/day-68/terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "project_name" {
+  type    = string
+  default = "terraweek"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ingress_ports" {
+  type    = list(number)
+  default = [22, 80]
+}
+
+variable "key_name" {
+  description = "AWS EC2 Key Pair"
+  type        = string
+}
